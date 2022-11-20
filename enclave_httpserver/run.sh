5b6fa884b9bc87a8f8e804264e912705d12b5e4d
JinggaoQ <88450027+JinggaoQ@users.noreply.github.com>
2022-11-20 10:43:30 +0800

    Create run.sh

---
+++ enclave_httpserver/run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Assign an IP address to local loopback 
+ip addr add 127.0.0.1/32 dev lo
+
+ip link set dev lo up
+
+/httptest &
+socat VSOCK-LISTEN:8001,fork,reuseaddr TCP:127.0.0.1:8888
